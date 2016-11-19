abcf705c8161fd41fa83003ccc5516cf2d0a9a21
ImmersiveS <seruy5432@gmail.com>
2016-11-19 14:04:24 +0200

    Modified sketch of warehouse

---
+++ src/Warehouse.h
@@ -0,0 +1,37 @@
+//
+// Created by User on 19.11.2016.
+//
+
+#ifndef WAREHOUSE_WORK_WEARHOUSE_H
+#define WAREHOUSE_WORK_WEARHOUSE_H
+
+#include <iostream>
+#include <vector>
+#include "Product.h"
+#include "Invoice.h"
+#include "Request.h"
+#include "Contract.h"
+#include "Supplier.h"
+#include "Client.h"
+
+class Warehouse
+{
+public:
+private:
+    std::string name;
+    std::vector<Product> products;
+    class Accounting
+    {
+    public:
+        void sendRequest(Supplier supplier, Request request);
+        void payInvoice(Invoice invoice);
+        void sendInvoice(Client client, Invoice invoice);
+    private:
+        std::vector<Invoice> invoices;
+        std::vector<Request> requests;
+        std::vector<Contract> contracts;
+    };
+};
+
+
+#endif //WAREHOUSE_WORK_WEARHOUSE_H
