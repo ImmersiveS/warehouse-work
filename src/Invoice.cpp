e99d1e92882fd3943274ed40c6c220d034ea4c25
ImmersiveS <seruy5432@gmail.com>
2016-11-19 15:52:00 +0200

    Implemented invoice class

--- src/Invoice.cpp
+++ src/Invoice.cpp
@@ -3,3 +3,35 @@
 //
 
 #include "Invoice.h"
+
+const std::vector<Product> &Invoice::getProducts() const {
+    return products;
+}
+
+void Invoice::setProducts(const std::vector<Product> &products) {
+    Invoice::products = products;
+}
+
+int Invoice::getCost() const {
+    return cost;
+}
+
+void Invoice::setCost(int cost) {
+    Invoice::cost = cost;
+}
+
+bool Invoice::isIsPaid() const {
+    return isPaid;
+}
+
+void Invoice::setIsPaid(bool isPaid) {
+    Invoice::isPaid = isPaid;
+}
+
+const std::string &Invoice::getDateOfPaying() const {
+    return dateOfPaying;
+}
+
+void Invoice::setDateOfPaying(const std::string &dateOfPaying) {
+    Invoice::dateOfPaying = dateOfPaying;
+}
--- src/Invoice.h
+++ src/Invoice.h
@@ -4,10 +4,33 @@
 
 #ifndef WAREHOUSE_WORK_INVOCE_H
 #define WAREHOUSE_WORK_INVOCE_H
+#include <iostream>
+#include <vector>
+#include "Product.h"
 
+class Invoice {
+public:
+    const std::vector<Product> &getProducts() const;
 
-class Invoce {
+    void setProducts(const std::vector<Product> &products);
 
+    int getCost() const;
+
+    void setCost(int cost);
+
+    bool isIsPaid() const;
+
+    void setIsPaid(bool isPaid);
+
+    const std::string &getDateOfPaying() const;
+
+    void setDateOfPaying(const std::string &dateOfPaying);
+
+private:
+    std::vector<Product> products;
+    int cost;
+    bool isPaid;
+    std::string dateOfPaying;
 };
 
 
